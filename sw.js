b0585b1e8690a33c896887400e6ee059bb298183
RollaDev <88199723+RollaDev@users.noreply.github.com>
2021-07-30 16:13:18 -0700

    Add files via upload
    GUI prototype and PWA implementation

---
+++ sw.js
@@ -0,0 +1,33 @@
+const cacheName = 'rolla-static'
+const staticAssets = [
+  '/index.html',
+  '/main.css',
+  '/app.js',
+  '/manifest.webmanifest'
+]
+
+self.addEventListener('install', async (event) => {
+  const cache = await caches.open(cacheName)
+  await cache.addAll(staticAssets)
+  return self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(networkFirst(event.request))
+})
+
+async function networkFirst (req) {
+  const cache = await caches.open(cacheName)
+  try {
+    const fresh = await fetch(req)
+    await cache.put(req, fresh.clone())
+    return fresh
+  } catch (e) {
+    const cached = await cache.match(req)
+    return cached
+  }
+}
