1b8089905daa36a41a91db6132a09d1d34dc47d2
Friendseeker <66892505+Friendseeker@users.noreply.github.com>
2021-07-31 00:13:45 -0700

    fix(server.js): force cors middleware to be the first
    Tentative fix to enable cross-origin isolation, hence allow SharedArrayBuffer to be used

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname));
-
-app.use((req, res, next) => {
-    res.header('Cross-Origin-Opener-Policy', 'same-origin');
-    res.header('Cross-Origin-Embedder-Policy', 'require-corp');
-    res.header('Cross-Origin-Resource-Policy', 'cross-origin'); // might not be needed
+app.use(function(req, res, next) {
+    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
     next();
 });
 
+app.use(express.static(__dirname));
+
 
 const PORT = process.env.PORT || 8080;
-
 
 
 app.listen(PORT, function () {
     const host = 'localhost';
     console.log('listening on http://'+host+':'+PORT+'/');
 });
-
