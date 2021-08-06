4a3173dc28ef4febaf362c22600fb1632acb5106
J.T <jiahui.tan@alumni.ubc.ca>
2021-08-05 17:04:55 -0700

    fix(build.sh): refresh bash after rsync is upgraded

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@ lsb_release -a
 # Check which ubuntu version is being used
 rsync --version
 brew install rsync
-# Crude Fix
+# Crude Fix for rsync version
+. ~/.bashrc
+# To refresh bash session
 rsync -a --mkpath ./src ./site
