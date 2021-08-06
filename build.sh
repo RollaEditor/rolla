e63aa0079caf15c1773a195520bae60fe1f3a8a5
J.T <jiahui.tan@alumni.ubc.ca>
2021-08-05 17:15:40 -0700

    fix(build.sh, netlify.toml): select contents of folder instead

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
-lsb_release -a
+# depreciated due to taking too long
+# lsb_release -a
 # Check which ubuntu version is being used
-rsync --version
-brew install rsync
+# rsync --version
+# brew install rsync
 # Crude Fix for rsync version
-. ~/.bashrc
+# . ~/.bashrc
 # To refresh bash session
-rsync -a --mkpath ./src ./site
+# rsync -a --mkpath ./src ./site
+
+mkdir site
+rsync -a -R src/ site
